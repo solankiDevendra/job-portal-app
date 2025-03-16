abfbb70254c5ac80e2340f996a43832768ba1aa8
solankiDevendra <solankidevendra833@gmail.com>
2025-03-16 12:44:16 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOprion = {
-  origin: "https://jobportal-anbi.onrender.com",
+  origin: "https://job-portal-59y6.onrender.com",
   credentials: true,
 };
 

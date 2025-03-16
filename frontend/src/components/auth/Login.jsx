3c9357afdc95ed5ef66a78f5e907328c8cc60d01
solankiDevendra <solankidevendra833@gmail.com>
2025-03-16 19:08:06 +0530

    Update Login.jsx

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -37,6 +37,7 @@ console.log("login in frontend");
     try {
       dispatch(setLoading(true));
       console.log("login in frontend in api");
+      console.log("call login api ",USER_API_END_POINT);
       const res = await axios.post('${USER_API_END_POINT}/login', input, {
         headers: { "Content-Type": "application/json" },
         withCredentials: true,

45eca6010baa041fda686cd06f559a6246763c6b
solankiDevendra <solankidevendra833@gmail.com>
2025-03-16 12:47:36 +0530

    Update constant.js

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,4 @@
-export const USER_API_END_POINT = "https://jobportal-anbi.onrender.com/user"
-export const JOB_API_END_POINT = "https://jobportal-anbi.onrender.com/job"
-export const APPLICATION_API_END_POINT = "https://jobportal-anbi.onrender.com/application"
-export const COMPANY_API_END_POINT = "https://jobportal-anbi.onrender.com/company"
+export const USER_API_END_POINT = "https://job-portal-59y6.onrender.com/user"
+export const JOB_API_END_POINT = "https://job-portal-59y6.onrender.com/job"
+export const APPLICATION_API_END_POINT = "https://job-portal-59y6.onrender.com/application"
+export const COMPANY_API_END_POINT = "https://job-portal-59y6.onrender.com/company"

e035ac03b7e325c6b781bca4f08115ebcdae4f27
solankiDevendra <solankidevendra833@gmail.com>
2025-03-17 12:38:09 +0530

    Update useGetAppliedJobs.jsx

--- frontend/src/hooks/useGetAppliedJobs.jsx
+++ frontend/src/hooks/useGetAppliedJobs.jsx
@@ -13,11 +13,6 @@ const useGetAppliedJobs = () => {
         const res = await axios.get(`${APPLICATION_API_END_POINT}/get`, {
           withCredentials: true,
         });
-
-        console.log(
-          "applied job in hooks ,",
-          JSON.stringify(res.data.application)
-        );
         if (res.data.success) {
           dispatch(setAllAppliedJobs(res.data.application));
         }
